2cc16875b215ad945c17378077168d59a9673c0c
DESKTOP-HV7NR7E\Alex <alexandre.charvet@epsi.fr>
2018-04-10 15:08:15 +0200

    no message

--- src/fr/GestionProjet/servlets/Connexion.java
+++ src/fr/GestionProjet/servlets/Connexion.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Connexion
  */
-@WebServlet("/Connexion")
+@WebServlet("")
 public class Connexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
